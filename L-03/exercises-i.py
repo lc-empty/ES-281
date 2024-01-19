766f51bffa7182852bfc0dd94112d108d4f73d73
qwik-zgheib <zgheibmossad1010@gmail.com>
2024-01-19 10:12:23 -0500

    feat: add exercises part i (1 - 10)

---
+++ L-03/exercises-i.py
@@ -0,0 +1,39 @@
+# -------------------------------------------------------------------------------------------------------
+# 1. Escriba una función que reciba un conjunto de números y devuelva un conjunto con los números primos.
+
+# -------------------------------------------------------------------------------------------------------
+# 2. Escriba  una  función  que  reciba  un  conjunto  de  palabras  y  devuelva  un  conjunto  con  las
+#  palabras  que  comienzan con una letra determinada.
+
+# -------------------------------------------------------------------------------------------------------
+# 3. Escriba una función que reciba un conjunto de números y devuelva un conjunto con los números que son
+#  divisibles por un número determinado.
+
+# -------------------------------------------------------------------------------------------------------
+# 4. Escriba una función que reciba dos conjuntos de números y devuelva un conjunto con los números que e
+# stán en ambos conjuntos.
+
+# -------------------------------------------------------------------------------------------------------
+# 5. Escriba una función que reciba dos conjuntos de números y devuelva un conjunto con los números que e
+# stán en el primer conjunto pero no en el segundo.
+
+# -------------------------------------------------------------------------------------------------------
+# 6. Escriba una función que reciba dos conjuntos de números y devuelva un conjunto con los números que e
+# stán en el segundo conjunto pero no en el primero.
+
+# -------------------------------------------------------------------------------------------------------
+# 7. Escriba una función que reciba un conjunto de palabras y devuelva un conjunto con las palabras que s
+# on anagramas.
+
+# -------------------------------------------------------------------------------------------------------
+# 8. Escriba una función que reciba un conjunto de palabras y devuelva un conjunto con las palabras que s
+# on palíndromos.
+
+# -------------------------------------------------------------------------------------------------------
+# 9. Escriba  una  función  que  reciba  un  conjunto  de  palabras  y  devuelva  un  conjunto  con  las
+#  palabras  que tienen una longitud determinada.
+
+# -------------------------------------------------------------------------------------------------------
+# 0. Escriba  una  función  que  reciba  un  conjunto  de  palabras  y  devuelva  un  conjunto  con  las
+#  palabras  que contienen una letra determinada.
+
