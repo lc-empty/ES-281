991684f1cde1764a005831ab3934c1b2439c8a6f
Isaias Ramos Lopez <155261549+qwik-zgheib@users.noreply.github.com>
2024-03-05 07:36:04 -0500

    Update lista_enlazada_circular.py

--- lista_enlazada_circular.py
+++ lista_enlazada_circular.py
@@ -9,7 +9,22 @@ class ListaEnlazadaCircular:
         self.primero = None;
 
 
-    def insertarFinal(self, dato):
+    # Insertar/Agregar
+    def insertarInicio(self, dato):
+        nuevo_nodo = Nodo(dato)
+        if self.primero is None:
+            self.primero = nuevo_nodo
+            nuevo_nodo.siguiente = nuevo_nodo
+        else:
+            nuevo_nodo.siguiente = self.primero
+            actual = self.primero
+            while actual.siguiente != self.primero:
+                actual = actual.siguiente
+            actual.siguiente = nuevo_nodo
+            self.primero = nuevo_nodo
+
+
+    def insertar(self, dato):
         nuevo_nodo = Nodo(dato);
         if self.primero is None:
             self.primero = nuevo_nodo;
@@ -22,6 +37,7 @@ class ListaEnlazadaCircular:
             nodo_actual.siguiente = nuevo_nodo;
 
 
+    # Mostrar
     def imprimir(self):
         if self.primero is None:
             print("La lista está vacía")
@@ -35,21 +51,21 @@ class ListaEnlazadaCircular:
             print()
 
 
-# --- crear ---
+# # --- crear ---
 lista = ListaEnlazadaCircular()
-lista.insertarFinal(1)
-lista.insertarFinal(2)
-lista.insertarFinal(3)
+lista.insertar(1)
+lista.insertar(2)
+lista.insertar(3)
 lista.imprimir()
 
 
 # --- agregar ---
+lista.insertarInicio(0)
+lista.imprimir()
 
 
 # --- modificar ---
 
 
 # --- eliminar ---
-
-
 
